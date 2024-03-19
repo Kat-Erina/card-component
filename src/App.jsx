459adde3cd5a03adb52e96173bad32ab1c79b427
Kat-Erina <nalchevanidzekaterina@gmail.com>
2024-03-19 15:38:57 +0400

    move Stylaed components into StyledComponents.js file

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,22 @@
-import styled from "styled-components";
+// import styled from "styled-components";
 import { createGlobalStyle } from "styled-components";
 import mobileImage from "./assets/image-header-mobile.jpg";
 
+import {
+  MainStyle,
+  SectionStyledComponent,
+  ImageSection,
+  Image,
+  OverlayComponent,
+  Section,
+  Header,
+  PStyled,
+  LiElement,
+  ListSection,
+  PInSList,
+  SpanInList,
+  Span,
+} from "./styled-components/styledComponents";
 const GlobalStyles = createGlobalStyle`
 * {padding:0;
 margin:0;
@@ -57,186 +72,3 @@ function App() {
 }
 
 export default App;
-
-const MainStyle = styled.main`
-  min-height: 100%;
-  width: 100%;
-  padding: 6rem 0;
-  background-color: #0a0c1c;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  @media screen and (min-width: 1400px) {
-    padding: 177px 150px;
-  }
-`;
-
-const SectionStyledComponent = styled.section`
-  display: flex;
-  flex-direction: column;
-  height: 100%;
-  width: 80%;
-  background-color: #1b1937;
-  border-radius: 0.5rem;
-  box-shadow: 0px 20px 20px -10px rgba(23, 25, 41, 0.2);
-  @media screen and (min-width: 950px) {
-    width: 75%;
-  }
-  @media screen and (min-width: 1000px) {
-    flex-direction: row-reverse;
-    width: 80%;
-  }
-`;
-
-const ImageSection = styled.section`
-  height: 16rem;
-  width: 100%;
-  position: relative;
-  @media screen and (min-width: 500px) {
-    height: 350px;
-  }
-  @media screen and (min-width: 1000px) {
-    height: auto;
-    flex: 1;
-  }
-`;
-
-const Image = styled.img`
-  display: block;
-  width: 100%;
-  height: 100%;
-`;
-
-const OverlayComponent = styled.div`
-  position: absolute;
-  top: 0;
-  left: 0;
-  width: 100%;
-  height: 100%;
-  background-color: #4f1574;
-  opacity: 0.6;
-`;
-
-const Section = styled.section`
-  padding: 40px 32px;
-  height: 100%;
-
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  flex: 1;
-  @media screen and (min-width: 1000px) {
-  }
-  @media screen and (min-width: 1400px) {
-    height: auto;
-    align-items: center;
-    justify-content: center;
-  }
-`;
-
-const Header = styled.h1`
-  color: white;
-  font-size: 28px;
-  font-style: normal;
-  font-weight: 700;
-  line-height: 2rem;
-  text-align: center;
-  padding: 10px;
-  margin-bottom: 1rem;
-  @media screen and (min-width: 750px) {
-    font-size: 3rem;
-    line-height: 4rem;
-  }
-  @media screen and (min-width: 1000px) {
-    font-size: 36px;
-    line-height: 2rem;
-    text-align: start;
-    padding-left: 0;
-  }
-  @media screen and (min-width: 1400px) {
-    width: 26rem;
-    line-height: 44px;
-  }
-`;
-
-const Span = styled.span`
-  color: #ab5cdb;
-`;
-
-const PStyled = styled.p`
-  display: flex;
-  color: #fff;
-  text-align: center;
-  font-family: Inter;
-  font-size: 15px;
-  font-style: normal;
-  font-weight: 400;
-  line-height: 25px;
-  margin-bottom: 40px;
-  overflow-wrap: break-word;
-  word-wrap: break-word;
-  @media screen and (min-width: 750px) {
-    font-size: 1.5rem;
-    line-height: 2rem;
-  }
-  @media screen and (min-width: 1000px) {
-    font-size: 15px;
-    line-height: 1.5;
-    text-align: start;
-    margin-bottom: 4rem;
-  }
-  @media screen and (min-width: 1400px) {
-    width: 26rem;
-    float: left;
-    line-height: 25px;
-    margin-bottom: 70px;
-  }
-`;
-
-const ListSection = styled.ul`
-  padding: 3.2rem, 2rem;
-  text-align: center;
-  @media screen and (min-width: 1000px) {
-    display: flex;
-    flex-direction: row;
-    gap: 4rem;
-    width: 100%;
-  }
-  @media screen and (min-width: 1400px) {
-    width: 26rem;
-  }
-`;
-
-const LiElement = styled.li`
-  padding-bottom: 24px;
-  list-style: none;
-`;
-
-const PInSList = styled.p`
-  font-size: 24px;
-  font-style: normal;
-  font-weight: 700;
-  line-height: normal;
-  color: white;
-  @media screen and (min-width: 750px) {
-    font-size: 2rem;
-  }
-  @media screen and (min-width: 1000px) {
-    font-size: 1.5rem;
-    text-align: start;
-  }
-`;
-const SpanInList = styled.span`
-  text-align: center;
-  font-family: "Lexend Deca";
-  font-size: 12px;
-  font-style: normal;
-  font-weight: 400;
-  line-height: 25px;
-  letter-spacing: 1px;
-  text-transform: uppercase;
-  color: white;
-  @media screen and (min-width: 1000px) {
-    font-size: 12px;
-  }
-`;
---
+++ src/styled-components/styledComponents.js
@@ -0,0 +1,182 @@
+import styled from "styled-components";
+
+export const MainStyle = styled.main`
+  min-height: 100%;
+  width: 100%;
+  padding: 6rem 0;
+  background-color: #0a0c1c;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  @media screen and (min-width: 1400px) {
+    padding: 11rem 9.5rem;
+  }
+`;
+
+export const SectionStyledComponent = styled.section`
+  display: flex;
+  flex-direction: column;
+  height: 100%;
+  width: 80%;
+  background-color: #1b1937;
+  border-radius: 0.5rem;
+  box-shadow: 0px 20px 20px -10px rgba(23, 25, 41, 0.2);
+  @media screen and (min-width: 950px) {
+    width: 75%;
+  }
+  @media screen and (min-width: 1000px) {
+    flex-direction: row-reverse;
+    width: 80%;
+  }
+`;
+
+export const ImageSection = styled.section`
+  height: 16rem;
+  width: 100%;
+  position: relative;
+  @media screen and (min-width: 500px) {
+    height: 350px;
+  }
+  @media screen and (min-width: 1000px) {
+    height: auto;
+    flex: 1;
+  }
+`;
+
+export const Image = styled.img`
+  display: block;
+  width: 100%;
+  height: 100%;
+`;
+
+export const OverlayComponent = styled.div`
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-color: #4f1574;
+  opacity: 0.6;
+`;
+
+export const Section = styled.section`
+  padding: 3.2rem 2rem;
+  height: 100%;
+
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  flex: 1;
+  @media screen and (min-width: 1000px) {
+  }
+  @media screen and (min-width: 1400px) {
+    height: auto;
+    align-items: center;
+    justify-content: center;
+  }
+`;
+
+export const Header = styled.h1`
+  color: white;
+  font-size: 1.75rem;
+  font-style: normal;
+  font-weight: 700;
+  line-height: 2rem;
+  text-align: center;
+  padding: 10px;
+  margin-bottom: 1rem;
+  @media screen and (min-width: 750px) {
+    font-size: 3rem;
+    line-height: 4rem;
+  }
+  @media screen and (min-width: 1000px) {
+    font-size: 2.25rem;
+    line-height: 2rem;
+    text-align: start;
+    padding-left: 0;
+  }
+  @media screen and (min-width: 1400px) {
+    width: 26rem;
+    line-height: 2.75rem;
+  }
+`;
+
+export const Span = styled.span`
+  color: #ab5cdb;
+`;
+
+export const PStyled = styled.p`
+  display: flex;
+  color: #fff;
+  text-align: center;
+  font-size: 1.06rem;
+  font-style: normal;
+  font-weight: 400;
+  line-height: 25px;
+  margin-bottom: 2.5rem;
+  overflow-wrap: break-word;
+  word-wrap: break-word;
+  @media screen and (min-width: 750px) {
+    font-size: 1.5rem;
+    line-height: 2rem;
+  }
+  @media screen and (min-width: 1000px) {
+    font-size: 15px;
+    line-height: 1.5;
+    text-align: start;
+    margin-bottom: 4rem;
+  }
+  @media screen and (min-width: 1400px) {
+    width: 26rem;
+
+    line-height: 25px;
+    margin-bottom: 70px;
+  }
+`;
+
+export const ListSection = styled.ul`
+  padding: 3.2rem, 2rem;
+  text-align: center;
+  @media screen and (min-width: 1000px) {
+    display: flex;
+    flex-direction: row;
+    gap: 4rem;
+    width: 100%;
+  }
+  @media screen and (min-width: 1400px) {
+    width: 26rem;
+  }
+`;
+
+export const LiElement = styled.li`
+  padding-bottom: 1.5rem;
+  list-style: none;
+`;
+
+export const PInSList = styled.p`
+  font-size: 1.5rem;
+  font-style: normal;
+  font-weight: 700;
+  line-height: normal;
+  color: white;
+  @media screen and (min-width: 750px) {
+    font-size: 2rem;
+  }
+  @media screen and (min-width: 1000px) {
+    font-size: 1.5rem;
+    text-align: start;
+  }
+`;
+export const SpanInList = styled.span`
+  text-align: center;
+  font-size: 12px;
+  font-style: normal;
+  font-weight: 400;
+  line-height: 25px;
+  letter-spacing: 1px;
+  text-transform: uppercase;
+  color: white;
+  @media screen and (min-width: 1000px) {
+    font-size: 12px;
+  }
+`;
